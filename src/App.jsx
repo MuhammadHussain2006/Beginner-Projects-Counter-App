068b6ade0eea76403a39afc289f6b4423e994df1
Hnoor0905-cloud <hnoor0905@gmail.com>
2025-11-21 03:52:46 +0500

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,18 @@
 import { useState } from "react";
 
 function App() {
-  const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = () => {
-    if (task.trim() === "") return;
-
-    setTasks([...tasks, task]);
-    setTask("");
-  };
-
-  const deleteTask = (index) => {
-    const updatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(updatedTasks);
-  };
+  const [count, setCount] = useState(0);
 
   return (
-    <div style={{
-      maxWidth: "400px",
-      margin: "50px auto",
-      padding: "20px",
-      border: "1px solid #ccc",
-      borderRadius: "10px",
-      textAlign: "center"
-    }}>
-      <h1>To-Do App</h1>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
 
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        placeholder="Enter a task..."
-        style={{
-          padding: "10px",
-          width: "100%",
-          marginBottom: "10px"
-        }}
-      />
-
-      <button onClick={addTask}>Add Task</button>
-
-      <ul style={{ marginTop: "20px", textAlign: "left" }}>
-        {tasks.map((item, index) => (
-          <li key={index} style={{ marginBottom: "10px" }}>
-            {item}
-            <button
-              onClick={() => deleteTask(index)}
-              style={{
-                marginLeft: "10px",
-                color: "white",
-                background: "red",
-                border: "none",
-                padding: "5px 10px",
-                cursor: "pointer"
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <br /><br />
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <br /><br />
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
