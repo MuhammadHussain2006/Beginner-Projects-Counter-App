e34e0de4e3f2ed36e18d1a05c00d85ac31cbfbca
Hnoor0905-cloud <hnoor0905@gmail.com>
2025-11-21 04:22:19 +0500

    content change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // include ts, tsx just in case
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
